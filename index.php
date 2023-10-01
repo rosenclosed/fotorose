730dcb01ab30a4d7c4fa671b59c935c5a9b871ba
rosenclosed <rose@rosedev.de>
2023-10-01 23:16:23 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!<![endif]-->
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>rosefoto.de - Aktionsfotos aus München & Umgebung</title>
+        <meta name="description" content="Aktionsfotos aus München und Umgebung">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/style/style.css">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="/script/site.js" async defer></script>
+
+        <main>
+        <h1>Willkommen bei rosefoto.de</h1>
+        <p>Keine Sorge! Die Seite wird irgendwann noch schöner, im Moment fehlen mir nur die Kapas dazu...</p>
+        <p>So lange ist das hier eher eine wenig organisierte Linkliste die du fröhlich durchklicken kannst.</p>
+        <br>
+        <h2>Hinweise zu den Fotos:</h2>
+        <h3>Lizenzen:</h3>
+        <p xmlns:cc="http://creativecommons.org/ns#">
+        Alle Fotos von mir, <a rel="cc:attributionURL dct:creator" property="cc:attributionName"
+            href="https://rosefoto.de">Rose Reed</a>, sind lizensiert nach <a
+            href="https://creativecommons.org/licenses/by-sa/4.0/deed.de?ref=chooser-v1" target="_blank"
+            rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution-ShareAlike 4.0 International<img
+                style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+                src="/img/cc/cc.svg?ref=chooser-v1"><img
+                style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+                src="/img/cc/by.svg?ref=chooser-v1"><img
+                style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+                src="/img/cc/sa.svg?ref=chooser-v1"></a>
+    </p>
+    <h3>Urheberrecht:</h3>
+    <p>
+    Das Urheberrecht gilt weiterhin.
+    </p>
+    <h3>Huch, ich werd ja weitergeleitet?!</h3>
+    <p>
+        Die Fotos werden auf einem separaten, von der Firma Hetzner betriebenen Server bereitgestellt. Alle Fotos die von der Domain <a href="https://fotos.rosefoto.de">fotos.rosefoto.de</a> stammen, sind von mir, alle anderen nicht. <strong>Sollte sich daran etwas ändern, würde ich euch hier in roter Fettschrift informieren.</strong>
+    </p>
+    <h3>Gibt es die Bilder auch unverpixelt?</h3>
+    <p>
+        <strong>NEIN!</strong> Selbst wenn ich wollte, könnte ich euch nach dem Veröffentlichen keine unverpixelten Versionen zur Verfügung stellen, da diese mit sicheren Methoden von allen Speichermedien geschreddert wurden.
+    </p>
+        <br>
+        <h2>Aktuelle Alben:</h2>
+        <ul>
+            <li><a href="https://fotos.rosefoto.de/apps/memories/a/XSBKP1gZowZfI4isj1oEXtquirV79UyP">1000 Kreuze Marsch Oktober 2023</a></li>
+        </ul>
+        </main>
+    </body>
+</html>
